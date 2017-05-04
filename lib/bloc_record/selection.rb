c676a849546c933f2cbcbadfe4dd4ceaa69bf974
JD Gonzales <jd_gonzales@icloud.com>
2017-05-03 21:23:36 -0500

    completed assignment

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -129,6 +129,7 @@ module Selection
     unless start.is_a?(Integer) && batch_size.is_a?(Integer)
       raise ArgumentError.new('Arguments must be integers')
     end
+
     rows = connection.execute <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       LIMIT #{batch_size} OFFSET #{start};
@@ -140,7 +141,93 @@ module Selection
 
   end
 
+  def where(*args)
+    if args.count > 1
+      expression = args.shift
+      params = args
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map{ |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }.join(" and ")
+      end
+    end
+
+    sql == <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{expression};
+    SQL
+
+    rows = connection.execute(sql, params)
+    rows_to_array(rows)
+  end
+
+  def order(*args)
+    orders = []
+
+    if args
+      args.each do |arg|
+        if arg.kind_of? String
+          orders << assend_descend(arg)
+        elsif arg.kind_of? Symbol
+          orders << assend_descend(arg.to_s)
+        end
+      end
+    end
+
+    orders.join(',')
+
+
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table}
+      ORDER BY #{orders};
+    SQL
+
+    rows_to_array(rows)
+  end
+
+  def join(*args)
+    if args.count > 1
+      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+      rows = connection.execute <<-SQL
+         SELECT * FROM #{table} #{joins}
+       SQL
+    else
+      case args.first
+      when String
+        rows = connection.execute <<-SQL
+           SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+         SQL
+      when Symbol
+        rows = connection.execute <<-SQL
+           SELECT * FROM #{table}
+           INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+         SQL
+      when Hash
+        key = args.first.keys[0]
+        value = args.first.values[0]
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{key} ON #{key}.#{table}_id = #{table}.id
+          INNER JOIN #{value} ON #{value}.#{key}_id = #{key}.id
+        SQL
+      end
+    end
+
+    rows_to_array(rows)
+  end
+
   private
+
+  def assend_descend(string)
+    if string.include?(" asc") || string.include?(" ASC") || string.include?(" desc") || string.include?(" DESC")
+      return string
+    else
+      string << " ASC"
+    end
+  end
 
   def init_object_from_row(row)
     if row
