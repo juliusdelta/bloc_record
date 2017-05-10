8ef9c024532c82352a8eb760763cbbbed02fa729
JD Gonzales <jd_gonzales@icloud.com>
2017-05-09 21:02:13 -0500

    completed checkpoint 5 assignment

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -24,8 +24,20 @@ module Persistence
     
     true
   end
+
+  def update_attribute(attribute, value)
+    self.class.update(self.id, {attribute => value})
+  end
+
+  def update_attributes(updates)
+   self.class.update(self.id, updates)
+  end
   
   module ClassMethods
+    def update_all(updates)
+      update(nil, updates)
+    end
+
     def create(attrs)
       attrs = BlocRecord::Utility.convert_keys(attrs)
       attrs.delete "id"
@@ -38,6 +50,43 @@ module Persistence
       data = Hash[attributes.zip attrs.values]
       data["id"] = connection.execute("SELECT last_insert_rowid();")[0][0]
       new(data)
+    end
+
+    def method_missing(m, *args, &block)
+      if m == :update_name
+        return update_attribute(:name, "#{args[0]}")
+      else
+        return "There is no #{m} method!"
+      end
+    end
+
+    def update(ids, updates)
+      if ids.class == Array
+        updates_array = []
+        ids.each_with_index do |id, index|
+          update(id, updates[index])
+        end
+      else
+        updates = BlocRecord::Utility.convert_keys(updates)
+        updates.delete "id"
+
+        updates_array = updates.map{ |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }
+      end
+
+      if ids.class == Fixnum
+        where_clause = "WHERE id = #{ids};"
+      elsif ids.class == Array
+        where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+      else
+        where_clause = ";"
+      end
+
+      connection.execute <<-SQL
+        UPDATE #{table}
+        SET #{updates_array * ","} #{where_clause}
+      SQL
+
+      true
     end
   end
 end
